0328aa45659bc4049b481eef0862b311ee0dd4f5
yrkim005 <yrkim05@intercoax.com>
2024-08-14 18:08:06 +0900

    modify command for 2046

--- drivers/input/touchscreen/ads7846.c
+++ drivers/input/touchscreen/ads7846.c
@@ -182,14 +182,21 @@ struct ads7846 {
 #define	READ_12BIT_DFR(x, adc, vref) (ADS_START | ADS_A2A1A0_d_ ## x \
 	| ADS_12_BIT | ADS_DFR | \
 	(adc ? ADS_PD10_ADC_ON : 0) | (vref ? ADS_PD10_REF_ON : 0))
+#if 1	//24.08.14 YRKIM
+#define	READ_Y(vref)	(READ_12BIT_DFR(y,  0, vref))
+#define	READ_Z1(vref)	(READ_12BIT_DFR(z1, 0, vref))
+#define	READ_Z2(vref)	(READ_12BIT_DFR(z2, 0, vref))
 
+#define	READ_X(vref)	(READ_12BIT_DFR(x,  0, vref))
+#define	PWRDOWN		(READ_12BIT_DFR(y,  0, 0))	/* LAST */
+#else
 #define	READ_Y(vref)	(READ_12BIT_DFR(y,  1, vref))
 #define	READ_Z1(vref)	(READ_12BIT_DFR(z1, 1, vref))
 #define	READ_Z2(vref)	(READ_12BIT_DFR(z2, 1, vref))
 
 #define	READ_X(vref)	(READ_12BIT_DFR(x,  1, vref))
 #define	PWRDOWN		(READ_12BIT_DFR(y,  0, 0))	/* LAST */
-
+#endif
 /* single-ended samples need to first power up reference voltage;
  * we leave both ADC and VREF powered
  */
