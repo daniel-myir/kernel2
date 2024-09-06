b922682bf89146f37d488ebc41f45f111165f52f
yrkim005 <yrkim05@intercoax.com>
2024-09-06 10:48:01 +0900

    power down with z1

--- drivers/input/touchscreen/ads7846.c
+++ drivers/input/touchscreen/ads7846.c
@@ -191,7 +191,7 @@ struct ads7846 {
 #define	READ_Z2(vref)	(READ_12BIT_DFR(z2, 1, vref))
 
 #define	READ_X(vref)	(READ_12BIT_DFR(x,  1, vref))
-#define	PWRDOWN		(READ_12BIT_DFR(y,  0, 0))	/* LAST */
+#define	PWRDOWN		(READ_12BIT_DFR(z1,  0, 0))	/* LAST */
 #else
 #define	READ_Y(vref)	(READ_12BIT_DFR(y,  1, vref))
 #define	READ_Z1(vref)	(READ_12BIT_DFR(z1, 1, vref))
